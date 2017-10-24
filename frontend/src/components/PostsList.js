a281988e2efd01789a0a84741c7fe0c63820a303
yuriko <ytamura@gmail.com>
2017-10-23 17:56:05 -0700

    initial posts sorting

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Loading from 'react-loading';
+import sortBy from 'sort-by';
 
 class PostsList extends Component {
+  state = {
+    sortPostsBy: '-voteScore',
+  }
+
   render() {
+    const { sortPostsBy } = this.state
     const { posts } = this.props;
+    let postsToShow = posts.filter((post) => post.deleted === false)
+    postsToShow.sort(sortBy(sortPostsBy));
 
     return (
       <div>
@@ -12,15 +20,13 @@ class PostsList extends Component {
         {posts === undefined
           ? <Loading delay={200} type='spin' color='#222' className='loading'/>
           : <div className='posts-list'>
-            {posts.filter((post) =>
-              post.deleted === false
-            ).map((post) => (
-              <div key={post.id} className="post-list-item">
-                <h4>{post.category}</h4>
-                <h4>[{post.voteScore}] {post.title} by {post.author}</h4>
-                <p>[{Date(post.timestamp)}] {post.body}</p>
-              </div>)
-            )}
+              {postsToShow.map((post) => (
+                <div key={post.id} className="post-list-item">
+                  <h4>{post.category}</h4>
+                  <h4>[{post.voteScore}] {post.title} by {post.author}</h4>
+                  <p>[{Date(post.timestamp)}] {post.body}</p>
+                </div>)
+              )}
             </div>
         }
       </div>
